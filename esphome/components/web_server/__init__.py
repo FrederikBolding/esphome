5fde0580be1778c4d6939b9f267d9baa409cc5e7
Tomasz Duda <tomaszduda23@gmai.com>
2024-09-30 22:48:32 +0200

    move component_iterator to new component

--- esphome/components/web_server/__init__.py
+++ esphome/components/web_server/__init__.py
@@ -37,7 +37,7 @@ from esphome.core import CORE, CoroPriority, coroutine_with_priority
 import esphome.final_validate as fv
 from esphome.types import ConfigType
 
-AUTO_LOAD = ["json", "web_server_base"]
+AUTO_LOAD = ["json", "web_server_base", "component_iterator"]
 
 CONF_SORTING_GROUP_ID = "sorting_group_id"
 CONF_SORTING_GROUPS = "sorting_groups"
