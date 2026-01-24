89850d0ee8607156e45d9afc88491abe1b96d04a
Frederik Bolding <frederik.bolding@gmail.com>
2026-01-24 23:46:48 +0100

    Defer TinyUSB setup

--- esphome/components/tinyusb/tinyusb_component.cpp
+++ esphome/components/tinyusb/tinyusb_component.cpp
@@ -22,11 +22,13 @@ void TinyUSB::setup() {
       .external_phy = false,
   };
 
-  esp_err_t result = tinyusb_driver_install(&this->tusb_cfg_);
-  if (result != ESP_OK) {
-    ESP_LOGE(TAG, "TinyUSB driver install failed; error %s", esp_err_to_name(result));
-    this->mark_failed(esp_err_to_name(result));
-  }
+  this->set_timeout(1000 * 10, [this] {
+    esp_err_t result = tinyusb_driver_install(&this->tusb_cfg_);
+    if (result != ESP_OK) {
+      ESP_LOGE(TAG, "TinyUSB driver install failed; error %s", esp_err_to_name(result));
+      this->mark_failed(esp_err_to_name(result));
+    }
+  })
 }
 
 void TinyUSB::dump_config() {
