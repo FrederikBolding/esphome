9ad452fe4a6ce52d769052825bf065d5cbc655ad
Frederik Bolding <frederik.bolding@gmail.com>
2026-01-24 23:36:44 +0100

    Temporarily do init in dump_config

--- esphome/components/tinyusb/tinyusb_component.cpp
+++ esphome/components/tinyusb/tinyusb_component.cpp
@@ -22,21 +22,19 @@ void TinyUSB::setup() {
       .external_phy = false,
   };
 
+  /**esp_err_t result = tinyusb_driver_install(&this->tusb_cfg_);
+  if (result != ESP_OK) {
+    ESP_LOGE(TAG, "TinyUSB driver install failed; error %s", esp_err_to_name(result));
+    this->mark_failed(esp_err_to_name(result));
+  }**/
+}
+
+void TinyUSB::dump_config() {
+  // TEMP: Do init here
   esp_err_t result = tinyusb_driver_install(&this->tusb_cfg_);
   if (result != ESP_OK) {
     ESP_LOGE(TAG, "TinyUSB driver install failed; error %s", esp_err_to_name(result));
     this->mark_failed(esp_err_to_name(result));
-  }
-}
-
-void TinyUSB::dump_config() {
-  // TEMP: Retry
-  if (this->is_failed()) {
-    esp_err_t result = tinyusb_driver_install(&this->tusb_cfg_);
-    if (result != ESP_OK) {
-      ESP_LOGE(TAG, "TinyUSB driver install failed; error %s", esp_err_to_name(result));
-      this->mark_failed(esp_err_to_name(result));
-    }
   }
   ESP_LOGCONFIG(TAG,
                 "TinyUSB:\n"
