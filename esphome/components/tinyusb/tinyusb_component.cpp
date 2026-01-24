2994110d7f601f2e26693b7bd12ef74fb7d48550
Frederik Bolding <frederik.bolding@gmail.com>
2026-01-24 23:40:09 +0100

    Revert

--- esphome/components/tinyusb/tinyusb_component.cpp
+++ esphome/components/tinyusb/tinyusb_component.cpp
@@ -22,20 +22,14 @@ void TinyUSB::setup() {
       .external_phy = false,
   };
 
-  /**esp_err_t result = tinyusb_driver_install(&this->tusb_cfg_);
-  if (result != ESP_OK) {
-    ESP_LOGE(TAG, "TinyUSB driver install failed; error %s", esp_err_to_name(result));
-    this->mark_failed(esp_err_to_name(result));
-  }**/
-}
-
-void TinyUSB::dump_config() {
-  // TEMP: Do init here
   esp_err_t result = tinyusb_driver_install(&this->tusb_cfg_);
   if (result != ESP_OK) {
     ESP_LOGE(TAG, "TinyUSB driver install failed; error %s", esp_err_to_name(result));
     this->mark_failed(esp_err_to_name(result));
   }
+}
+
+void TinyUSB::dump_config() {
   ESP_LOGCONFIG(TAG,
                 "TinyUSB:\n"
                 "  Product ID: 0x%04X\n"
